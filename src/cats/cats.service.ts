f619db963f4d7dd7486d54452acf4acd32b0468c
richard.liao <richard.liao@tpisoftware.com>
2023-04-26 21:10:33 +0800

    chore: 建立 cats api

---
+++ src/cats/cats.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class CatsService {
+  //   className: ClassName_singularDto[] = [{
+  //     id: '1',
+  //     name: 'A SPA app',
+  //   },
+  //   {
+  //     id: '2',
+  //     name: 'A Nest API',
+  //   }];
+}
